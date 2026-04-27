992ed2b0cc72ecd544bdf69b10100b602353002f
HoangAnh-ToiChoi <hoanganh281205@gmail.com>
2026-04-27 20:45:11 +0700

    Finshing my homework

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+    base: "/f8-zoom-module-1",
+
+    root: "src",
+
+    publicDir: "../public",
+
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+                practice: path.resolve(__dirname, "src/practice.html"),
+                blog: path.resolve(__dirname, "src/blog.html"),
+                contact: path.resolve(__dirname, "src/contact.html"),
+            },
+        },
+    },
+
+    plugins: [ViteEjsPlugin()],
+});
